4271e81bddc63839311d590d6d9d36d689872964
DuvanMotavita <duvanfel1@yahoo.com.co>
2025-06-23 21:25:33 -0500

    Added a lot of logic for details and lists

--- src/main/java/com/duvan/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/duvan/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -13,7 +13,8 @@ public class UserController {
     
     @GetMapping("/details")
     public String details(Model model) {
-         User user = new User("Duvan", "Motavita");
+         User user = new User("Duvan", "Motavita",null);
+         user.setEmail("duvanmotavita@gmail.com");
         model.addAttribute("title", "Hola Mundo Spring Boot");
         model.addAttribute("user", user);
         return "details";
--- src/main/java/com/duvan/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/duvan/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,6 +1,9 @@
 package com.duvan.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.ui.Model;
@@ -9,19 +12,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.duvan.curso.springboot.webapp.springboot_web.controllers.models.User;
+import com.duvan.curso.springboot.webapp.springboot_web.controllers.models.dto.UserDto;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
     
     @GetMapping("/details")
-    public Map<String, Object> details(Model model) {
-        User user = new User("Duvan", "Motavita");
-        Map<String, Object> body = new HashMap<>();
-        body.put("title", "Hola Mundo Spring Boot");
-        body.put("user", user);
-        return body;
+    public UserDto details(Model model) {
+        UserDto userDto = new UserDto();
+        User user = new User("Duvan", "Motavita",null);
+        userDto.setUser(user);
+        userDto.setTitle("Hola Mundo Spring Boot");
+        // Map<String, Object> body = new HashMap<>();
+        // body.put("title", "Hola Mundo Spring Boot");
+        // body.put("user", user);
+        return userDto;
     }
+    @GetMapping("/list")    
+    public List<User> list(){
+        User user = new User("Duvan", "Motavita",null);
+        User user2 = new User("Pepe", "Doe",null);
+        User user3= new User("Sari", "Motavita",null);
+
+        List<User> users = Arrays.asList(user,user2,user3);
+        // List<User> users = new ArrayList<>();
+        // users.add(user);
+        // users.add(user2);
+        // users.add(user3);
+
+        return users;
+
+    }
+
 
 }
  
